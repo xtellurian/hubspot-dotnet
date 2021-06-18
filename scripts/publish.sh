fdf4cd83d849778937f42237df971ddcbe9368b2
Rian Finnegan <rian@amphoradata.com>
2021-06-18 12:17:41 +1000

    update publish script version

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,4 +4,4 @@ key=$(cat .secret)
 
 cd ../src/hubspotdotnet
 dotnet build -c Release
-dotnet nuget push bin/Release/xtellurian.HubSpot.1.0.3.nupkg -k $key -s https://api.nuget.org/v3/index.json
+dotnet nuget push bin/Release/xtellurian.HubSpot.1.0.4.nupkg -k $key -s https://api.nuget.org/v3/index.json
